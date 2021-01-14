b5b7ea3bb445fccd6fdc96e414325a260cd522ae
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-01-14 16:29:30 +0100

    add jquery and dynamically change div

---
+++ resources/js/homesaves.js
@@ -0,0 +1,7 @@
+//go to registerloginpage
+$(document).ready(function(){
+   $(".button1").click(function(){
+       $('.container main').load("registerAndLogin.html");
+       $('.container main').css('padding-left','20%');
+   })
+});
