8c769d9dc3c0501aa91b9ee8551384bc6dcd9bd8
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-01-15 01:45:50 +0100

    add responsive menu to mainpage

--- resources/js/homesaves.js
+++ resources/js/homesaves.js
@@ -2,11 +2,13 @@
 $(document).ready(function(){
    $("#getStarted").click(function(){
        $('main').load("register.html");
+        $('footer').html("")
    })
 });
 $(document).ready(function(){
    $("#login").click(function(){
        $('main').load("login.html");
+        $('footer').html("")
    })
 });
 
@@ -16,5 +18,11 @@ $(document).ready(function(){
        $('main').load("mainPage.html");
        $('footer').html("")
        
+   })
+});
+
+$(document).ready(function(){
+   $("#logout").click(function(){
+       $('main').load("login.html"); 
    })
 });
