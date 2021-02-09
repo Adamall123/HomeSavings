405e2ac2a40e75f2c8986591897ae481a855e822
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-02-09 07:06:55 +0100

    add check if written category exists

--- addnew.php
+++ addnew.php
@@ -2,23 +2,80 @@
 	require_once "connect.php";
  	session_start();
 	$user_loggedin_id =  $_SESSION['id'];
-	$connection = new mysqli($host, $db_user,$db_password,$db_name);
-	if(isset($_POST['income'])){
-		$new_income=$_POST['income'];
-		mysqli_query($connection,"INSERT INTO incomes_category_assigned_to_users VALUES(NULL,'$user_loggedin_id','$new_income')");
-		unset($_POST['income']);		
-	}
+	try{
+		$connection = new mysqli($host, $db_user,$db_password,$db_name);
+		if($connection->connect_errno!=0){
+				throw new Exception(mysqli_connect_errno());
+		}else{
+			if(isset($_POST['income'])){
+			$all_OK = true;	
+			$new_income=$_POST['income'];
+			$result = $connection->query("SELECT name FROM incomes_category_assigned_to_users WHERE name='$new_income' AND user_id = '$user_loggedin_id'");
+			if(!$result) throw new Exception($connection->error);
+			$amount_of_category_income = $result->num_rows;
+			if($amount_of_category_income > 0){
+				$all_OK = false;
+				echo '<script type="text/javascript">alert("The written category exists in your app!");</script>';
+			}
+			if($all_OK){
+				if($connection->query("INSERT INTO incomes_category_assigned_to_users VALUES(NULL,'$user_loggedin_id','$new_income')")){
+					echo '<script type="text/javascript">alert("The new income has been created");</script>';
+					//set session variable and than display it 
+				}
+				unset($_POST['income']);	
+			}	
+			}
+			/*
+			if button not pressed then this Exeption is working 
+			what if button clicked and did not works
+			else {
+					throw new Exception($connection->error);
+				}
+			*/
+			if(isset($_POST['expence'])){
+			$all_OK = true;	
+			$new_expence=$_POST['expence'];
+			//find if new_income exists and if not than insert into 
+			$result = $connection->query("SELECT name FROM expenses_category_assigned_to_users WHERE name='$new_expence' AND user_id = '$user_loggedin_id'");
+			if(!$result) throw new Exception($connection->error);
+			$amount_of_category_income = $result->num_rows;
+			if($amount_of_category_income > 0){
+				$all_OK = false;
+				echo '<script type="text/javascript">alert("The written category exists in your app!");</script>';
+			}
+			if($all_OK){
 				
-	if(isset($_POST['expence'])){
-		$new_expence=$_POST['expence'];
-		mysqli_query($connection,"INSERT INTO expenses_category_assigned_to_users VALUES(NULL,'$user_loggedin_id','$new_expence')");	
-		unset($_POST['expence']);		
-	}
-	
-	if(isset($_POST['payment'])){
-		$new_payment_method=$_POST['payment'];
-		mysqli_query($connection,"INSERT INTO payment_methods_assigned_to_users VALUES(NULL,'$user_loggedin_id','$new_payment_method')");	
-		unset($_POST['payment']);		
+				if($connection->query("INSERT INTO expenses_category_assigned_to_users VALUES(NULL,'$user_loggedin_id','$new_expence')")){
+					echo '<script type="text/javascript">alert("The new income has been created");</script>';
+					//set session variable and than display it 
+				}
+				unset($_POST['expence']);	
+			}	
+			}
+			if(isset($_POST['payment'])){
+			$all_OK = true;
+			$new_payment_method=$_POST['payment'];
+			//find if new_income exists and if not than insert into 
+			$result = $connection->query("SELECT name FROM payment_methods_assigned_to_users WHERE name='$new_payment_method' AND user_id = '$user_loggedin_id'");
+			if(!$result) throw new Exception($connection->error);
+			$amount_of_category_income = $result->num_rows;
+			if($amount_of_category_income > 0){
+				$all_OK = false;
+				echo '<script type="text/javascript">alert("The written category exists in your app!");</script>';
+			}
+			if($all_OK){
+				
+				if($connection->query("INSERT INTO payment_methods_assigned_to_users VALUES(NULL,'$user_loggedin_id','$new_payment_method')")){
+					echo '<script type="text/javascript">alert("The new income has been created");</script>';
+					//set session variable and than display it 
+				}
+				unset($_POST['payment']);	
+			}		
+			}
+			$connection->close();
+		}
+	}catch(Exception $e){
+		
 	}
 	
 	header('location:settings.php');
--- edit.php
+++ edit.php
@@ -2,30 +2,37 @@
 	require_once "connect.php";
  	session_start();
 	$user_loggedin_id =  $_SESSION['id'];
-	$connection = new mysqli($host, $db_user,$db_password,$db_name);
 	
-	if(isset($_POST['editincome'])){
-		$income_id=$_GET['id'];
-		$edit_income=$_POST['editincome'];
-		mysqli_query($connection,"update incomes_category_assigned_to_users set name='$edit_income' where user_id='$user_loggedin_id' AND id= '$income_id'");
-		unset($_POST['editincome']);		
+	try{
+		$connection = new mysqli($host, $db_user,$db_password,$db_name);
+		if($connection->connect_errno!=0){
+				throw new Exception(mysqli_connect_errno());
+		}else{
+			if(isset($_POST['editincome'])){
+			$income_id=$_GET['id'];
+			$edit_income=$_POST['editincome'];
+			//find if edit_income exists and if not than update 
+			mysqli_query($connection,"update incomes_category_assigned_to_users set name='$edit_income' where user_id='$user_loggedin_id' AND id= '$income_id'");
+			unset($_POST['editincome']);		
+			}
+			if(isset($_POST['editexpence'])){
+			$expence_id=$_GET['id'];
+			$edit_expence=$_POST['editexpence'];
+			//find if edit_expence exists and if not than update 
+			mysqli_query($connection,"UPDATE expenses_category_assigned_to_users SET name='$edit_expence' WHERE user_id='$user_loggedin_id' AND id= '$expence_id'");
+			unset($_POST['editexpence']);		
+			}
+			if(isset($_POST['editpayment'])){
+			$payment_id=$_GET['id'];
+			$edit_payment_method=$_POST['editpayment'];
+			//find if edit_payment_method exists and if not than update 
+			mysqli_query($connection,"UPDATE payment_methods_assigned_to_users SET name='$edit_payment_method' WHERE user_id='$user_loggedin_id' AND id= '$payment_id'");
+			unset($_POST['editpayment']);		
+			}
+		}
+	}catch(Exception $e){
+		
 	}
-	
-	if(isset($_POST['editexpence'])){
-		$expence_id=$_GET['id'];
-		$edit_expence=$_POST['editexpence'];
-		mysqli_query($connection,"UPDATE expenses_category_assigned_to_users SET name='$edit_expence' WHERE user_id='$user_loggedin_id' AND id= '$expence_id'");
-		unset($_POST['editexpence']);		
-	}
-	
-	if(isset($_POST['editpayment'])){
-		$payment_id=$_GET['id'];
-		$edit_payment_method=$_POST['editpayment'];
-		mysqli_query($connection,"UPDATE payment_methods_assigned_to_users SET name='$edit_payment_method' WHERE user_id='$user_loggedin_id' AND id= '$payment_id'");
-		unset($_POST['editpayment']);		
-	}
-	
-
 	header('location:settings.php');
  
 ?>
